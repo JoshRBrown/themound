57073c1ab9af30eb63ee25a54b20c1f32d332a43
Josh Brown <jbrown016@gmail.com>
2018-06-20 11:08:10 -0400

    cloned project

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// Sport API key g853qfg7gsw5eztfz77uvyfx
+// CORS workaround http://my-little-cors-proxy.herokuapp.com/
+
+// startTheProgram(hierarchy);
+// nextStep(hierarchy);
+// gameSchedule(leagueScheduleAPI);
+masterPull();
+popTeamNames();
+getAndPopPlayerPositions();
+popGameSched();
+teamListener();
+positionListener();
+dateListener();
+// playerClick();
+// positionClicked();
+// playerClick($('[data-player-info-less]'));
+
+// clickListener($('[data-target-matchup]'));
+// getImages();
+// positionListener();
+
